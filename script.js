9812b486e53c54c8be6fd14d2a509eea8b9917be
Tess <tere0506@stud.kea.dk>
2022-10-16 10:52:12 +0200

    pages animation update

--- script.js
+++ script.js
@@ -3,4 +3,4 @@
 import { scroll, animate, timeline, stagger } from "https://cdn.skypack.dev/motion";
 
 console.log("hi");
-animate(".fadeIn", { opacity: 0, opacity: 1 }, { duration: 2, delay: stagger(0.2), easing: "ease-in-out" });
+animate(".fadeIn", { opacity: 0, opacity: 1 }, { duration: 1, delay: stagger(0.2), easing: "ease-in-out" });
